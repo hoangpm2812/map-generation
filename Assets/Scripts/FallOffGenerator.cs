a7eb1c7de1b9aa57d1b0674c96a515aa64b6da48
Hoang <hoangpm2@topica.edu.vn>
2018-04-09 12:04:16 +0700

    susscess project PCG - first commit

---
+++ Assets/Scripts/FallOffGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FallOffGenerator {
+
+    public static float[,] GenerateFallOffMap(int width, int height)
+    {
+        float[,] map = new float[width, height];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                float x = i / (float)height * 2 - 1;
+                float y = j / (float)width * 2 - 1;
+
+                float value = Mathf.Max (Mathf.Abs(x), Mathf.Abs(y));
+                map[i, j] = Evaluate(value);
+            }
+        }
+
+        return map;
+    }
+
+    static float Evaluate(float value)
+    {
+        float a = 3;
+        float b = 2.6f;
+        return Mathf.Pow(value, a) / (Mathf.Pow(value, a) + Mathf.Pow(b - b * value, a));
+    }
+
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MapGenerator : MonoBehaviour {
+
+    const int textureSize = 512;
+    const TextureFormat textureFormat = TextureFormat.RGB565;
+    public enum DrawMode { NoiseMap ,ColourMap, Mesh, FalloffMap};
+    public DrawMode drawMode;
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public int seed;
+
+    public bool useFalloff;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public Renderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    float[,] falloffMap;
+
+    private MapData mapdata;
+
+    public float minHeight
+    {
+        get
+        {
+            return 10f * meshHeightMultiplier * meshHeightCurve.Evaluate(0);
+        }
+    }
+
+    public float maxHeight
+    {
+        get
+        {
+            return 10f * meshHeightMultiplier * meshHeightCurve.Evaluate(1);
+        }
+    }
+
+    public Material terrainMaterial;
+
+    public Layer[] layers;
+
+
+    public void DrawMapInEditor()
+    {
+        MapData mapData = GenerateMapData();
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(mapData.colorMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColourMap(mapData.colorMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.FalloffMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(FallOffGenerator.GenerateFallOffMap(mapWidth, mapHeight)));
+        }
+    }
+
+    public MapData GenerateMapData()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for ( int x=0; x < mapWidth; x++)
+            {
+                if (useFalloff)
+                {
+                    noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - falloffMap[x, y]);
+                }
+                float currentHeight = noiseMap[x, y];
+                for (int i=0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        
+        UpdateMeshHeight(terrainMaterial, minHeight, maxHeight);
+
+        return new MapData(noiseMap, colourMap);
+        
+    }
+
+    void Awake()
+    {
+        falloffMap = FallOffGenerator.GenerateFallOffMap(mapWidth, mapHeight);
+    }
+
+    void Start()
+    {
+        GameObject meshObject = GameObject.FindGameObjectWithTag("meshObject");
+        meshObject.AddComponent<MeshCollider>();
+        mapdata = GenerateMapData();
+        falloffMap = FallOffGenerator.GenerateFallOffMap(mapWidth, mapHeight);
+        
+    }
+    void Update()
+    {
+        Texture2D texture = TextureGenerator.TextureFromColourMap(mapdata.colorMap, mapWidth, mapHeight);
+        meshRenderer.material.mainTexture = texture;
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        UpdateMeshHeight(terrainMaterial, minHeight, maxHeight);
+        falloffMap = FallOffGenerator.GenerateFallOffMap(mapWidth, mapHeight);
+    }
+
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
+    }
+
+    public struct MapData
+    {
+        public float[,] heightMap;
+        public Color[] colorMap;
+
+        public MapData (float[,] heightMap, Color[] colorMap)
+        {
+            this.heightMap = heightMap;
+            this.colorMap = colorMap;
+        }
+    }
+
+    Texture2DArray GenerateTextureArray(Texture2D[] textures)
+    {
+        Texture2DArray textureArray = new Texture2DArray(textureSize, textureSize, textures.Length, textureFormat, true);
+        for (int i = 0; i < textures.Length; i++)
+        {
+            textureArray.SetPixels(textures[i].GetPixels(), i);
+        }
+        textureArray.Apply();
+        return textureArray;
+    }
+
+    private void UpdateMeshHeight(Material material, float minHeight, float maxHeight)
+    {
+        material.SetInt("layerCount", layers.Length);
+        material.SetColorArray("baseColours", layers.Select(x => x.tint).ToArray());
+        material.SetFloatArray("baseStartHeights", layers.Select(x => x.startHeight).ToArray());
+        material.SetFloatArray("baseBlends", layers.Select(x => x.blendStrength).ToArray());
+        material.SetFloatArray("baseColourStrength", layers.Select(x => x.tintStrength).ToArray());
+        material.SetFloatArray("baseTextureScale", layers.Select(x => x.textureScale).ToArray());
+        Texture2DArray textureArray = GenerateTextureArray(layers.Select(x => x.texture).ToArray());
+        material.SetTexture("baseTextures", textureArray);
+
+
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
+    }
+
+    [System.Serializable]
+    public class Layer
+    {
+        public Texture2D texture;
+        public Color tint;
+        [Range(0,1)]
+        public float tintStrength;
+        [Range(0, 1)]
+        public float startHeight;
+        [Range(0, 1)]
+        public float blendStrength;
+        public float textureScale;
+    }
+}
